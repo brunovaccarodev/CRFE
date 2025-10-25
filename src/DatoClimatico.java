da11304578c4f1da2a0a7472dbc6a06d708de4f8
Bruno Vaccaro <brunovaccarodev@hotmail.com>
2025-10-25 09:07:40 -0300

    Update DatoClimatico.java
    Agregando comentarios

--- src/DatoClimatico.java
+++ src/DatoClimatico.java
@@ -1,4 +1,3 @@
-
 public class DatoClimatico {														//Definición de clase
 	private int idDato;																//Definición de atributos
 	private String FechaHora;                              
@@ -14,76 +13,58 @@ public class DatoClimatico {														//Definición de clase
 	public int obtenerIdDato () {													//Definición de métodos constructores
 		return idDato;
 	}
-	
 	public void establecerIdDato (int ID) {
 		idDato = ID;
 	}
-	
 	public String obtenerFechaHora () {
 		return FechaHora;
 	}
-	
 	public void establecerFechaHora (String fechaHora) {
 		FechaHora = fechaHora;
 	}
-	
 	public double obtenerTemperatura () {
 		return Temperatura;
 	}
-	
 	public void establecerTemperatura (double temperatura) {
 		Temperatura = temperatura;
 	}
-	
 	public double obtenerHumedad() {
 		return Humedad;
 	}
-	
 	public void establecerHumedad(double humedad) {
 		Humedad = humedad;
 	}
-	
 	public void establecerPrecipitacion(double precipitacion) {
 		Precipitacion = precipitacion;
 	}
-	
 	public double obtenerPrecipitacion() {
 		return Precipitacion;
 	}
-	
 	public double obtenerViento () {
 		return Viento;		
 	}
-	
 	public void establecerViento (double viento) {
 		Viento = viento;
 	}
-	
 	public double obtenerRadiacion() {
 		return Radiacion;
 	}
-	
 	public void establecerRadiacion(double radiacion) {
 		Radiacion = radiacion;
 	}
-	
 	public int obtenerIdEstacion() {
 		return idEstacion;
 	}
-	
 	public void establecerIdEstacion(int IDEstacion) {
 		idEstacion = IDEstacion;
 	}
-	
 	public int obtenerIdUsuario() {
 		return idUsuario;
 	}
-	
 	public void establecerIdUsuario(int IDUsuario) {
 		idUsuario = IDUsuario;
 	}
-	
-	public double registrarTemperatura() {											//Este método aún no se utiliza
+	public double registrarTemperatura() {											//Este método se puede utilizar a futuro, verifica los valores ingresados
 		double temp = obtenerTemperatura();
 		try {
 			if (temp > 0 && temp < 100) {
@@ -94,6 +75,4 @@ public class DatoClimatico {														//Definición de clase
 		}
 		return temp;
 	}
-	
 }
-
