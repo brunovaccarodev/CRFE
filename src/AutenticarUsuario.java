38117d1f1f807ec9763e90637ea436b6efc8f492
Bruno Vaccaro <brunovaccarodev@hotmail.com>
2025-10-25 09:00:03 -0300

    Update AutenticarUsuario.java
    Agregando comentarios

--- src/AutenticarUsuario.java
+++ src/AutenticarUsuario.java
@@ -1,53 +1,42 @@
-
-public class AutenticarUsuario {
-	private String Usuario;
+public class AutenticarUsuario {																//Definción de clase
+	private String Usuario;																		//Definición de atributos
 	private String Contraseña;
 	private int IntentosFallidos;
 	private String FechaUltimoLogin;
 	private String RolUsuario;
 	private boolean esAutenticado;
 	
-	public String obtenerUsuario() {
+	public String obtenerUsuario() {															//Definción de métodos constructores
 		return Usuario;
 	}
-	
 	public void EstablecerUsuario(String usuario) {
 		Usuario = usuario;
 	}
-	
 	public String obtenerContraseña () {
 		return Contraseña;
 	}
-	
 	public void establecerContraseña (String contraseña) {
 		Contraseña = contraseña;
 	}
-	
 	public int obtenerIntentosFallidos() {
 		return IntentosFallidos;
 	}
-	
 	public void establecerIntentosFallidos(int intentosFallidos) {
 		IntentosFallidos = intentosFallidos;
 	}
-	
 	public String obtenerFechaUltimoLogin() {
 		return FechaUltimoLogin;
 	}
-	
-	public void establecerFechaUltimoLogin (String fechaUltimoLogin) {						//Ver si es necesario
+	public void establecerFechaUltimoLogin (String fechaUltimoLogin) {						
 		FechaUltimoLogin = fechaUltimoLogin;
 	}
-	
 	public String obtenerRolUsuario() {
 		return RolUsuario;
 	}
-	
-	public void establecerRolUsuario(String rolUsuario) {										//Ver si es necesario
+	public void establecerRolUsuario(String rolUsuario) {									
 		RolUsuario = rolUsuario;
 	}
-	
-	public boolean esAutenticado() {
+	public boolean esAutenticado() {															//Método para realizar verificación de autenticación con base de datos
 		return esAutenticado;
 	}
 }
