b3083e5a7650fbfdcf9571089a28222b7c0e79b7
Bruno Vaccaro <brunovaccarodev@hotmail.com>
2025-10-25 09:13:52 -0300

    Update EstacionMeteorologica.java
    Agregando comentarios

--- src/EstacionMeteorologica.java
+++ src/EstacionMeteorologica.java
@@ -1,6 +1,5 @@
-
-public class EstacionMeteorologica {
-	private int IdEstacion;
+public class EstacionMeteorologica {														//Definición de clase
+	private int IdEstacion;																	//Definción de atributos
 	private String Nombre;
 	private String Ubicacion;
 	private double Latitud;
@@ -11,78 +10,60 @@ public class EstacionMeteorologica {
 	private String FechaDeInstalacion;
 	private boolean esActiva;
 	
-	public int obtenerIdEstacion () {
+	public int obtenerIdEstacion () {														//Definción de métodos constructores
 		return IdEstacion;
 	}
-	
 	public void establecerIdEstacion(int IDEstacion) {
 		IdEstacion = IDEstacion;
 	}
-	
 	public String obtenerNombre() {
 		return Nombre;
 	}
-	
 	public void establecerNombre(String nombre) {
 		Nombre = nombre;
 	}
-	
 	public String obtenerUbicacion() {
 		return Ubicacion;
 	}
-	
 	public void establecerUbicacion(String ubicacion) {
 		Ubicacion = ubicacion;
 	}
-	
 	public double obtenerLatitud() {
 		return Latitud;
 	}
-	
 	public void establecerLatitud(double latitud) {
 		Latitud = latitud;
 	}
-	
 	public double obtenerLongitud() {
 		return Longitud;
 	}
-	
 	public void establecerLogitud(double longitud) {
 		Longitud = longitud;
 	}
-	
 	public double obtenerAltitud() {
 		return Altitud;
 	}
-	
 	public void establecerAltitud(double altitud) {
 		Altitud = altitud;
 	}
-	
 	public String obtenerTipo() {
 		return Tipo;
 	}
-	
 	public void establecerTipo(String tipo) {
 		Tipo = tipo;
 	}
-	
 	public String obtenerEstado() {
 		return Estado;
 	}
-	
 	public void establecerEstado(String estado) {
 		Estado = estado;
 	}
-	
 	public String obtenerFechaDeInstalacion() {
 		return FechaDeInstalacion;
 	}
-	
 	public void establecerFechaDeInstalacion(String fechaDeInstalacion) {
 		FechaDeInstalacion = fechaDeInstalacion;
 	}
-	
 	public boolean esActiva () {
 		return esActiva;
 	}
