cc079f6d18f14aa9f0400b23748e445cb4f1da41
Bruno Vaccaro <brunovaccarodev@hotmail.com>
2025-10-25 09:38:29 -0300

    Update Principal.java
    Agregando comentarios

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,11 @@
 import javax.swing.JOptionPane;
 
-public class Principal {
-	public static void main (String [] args) {
-		UnidadesMedida unidadesMedida = new UnidadesMedida();
+public class Principal {																			//Definción de la clase
+	public static void main (String [] args) {														//Definición de método main
+		/*
+		Creando objetos
+		*/
+		UnidadesMedida unidadesMedida = new UnidadesMedida();			
 		ConfigurarUnidadesMedida configurarUnidadesMedida = new ConfigurarUnidadesMedida();
 		DatoClimatico datoClimatico = new DatoClimatico();
 		EstacionMeteorologica estacionMeteorologica = new EstacionMeteorologica();
@@ -13,6 +16,9 @@ public class Principal {
 		Reporte reporte = new Reporte();
 		Usuario usuario = new Usuario();
 		
+		/*
+		Instanciamiento de clases y llamadas a métodos del proyecto
+		*/
 		
 		double temperaturaCelsius = configurarUnidadesMedida.convertirUnidadTemperaturaCelsius(14);
 		double temperaturaFahrenheit = configurarUnidadesMedida.convertirUnidadTemperaturaFahrenheit(89);
@@ -45,7 +51,9 @@ public class Principal {
 		String nombreReporte = generadorReportes.obtenerNombreDeReporte();
 		boolean autenticacion = login.esAutenticado();
 		String fechaDeReporte = reporte.obtenerFechaDeCreacion();
-		
+		/* 
+		Verificación de funcionamiento por consola
+		*/
 		System.out.println("La conversion de grados Celsius a Fahrenheit es: : " + temperaturaCelsius);
 		System.out.println ("La conversion de grados Fahrenheit a Celsius es: " + temperaturaFahrenheit);
 		System.out.println ("El IDDato es: " + IDDato);
@@ -58,8 +66,5 @@ public class Principal {
 		System.out.println ("Se exporto el archivo a la ruta: " + ruta);
 		System.out.println ("Se ha generado el reporte con el ID : " + IDReporte + " con nombre: " + nombreReporte + " en la fecha: " + fechaDeReporte);
 		System.out.println ("El valor booleano que resulta de la autenticacion es: " + autenticacion);
-		
-		  
-	
 	}
 }
