11dc73ee444f154425cc56c82223ae4e80875ef9
Bruno Vaccaro <brunovaccarodev@hotmail.com>
2025-10-25 09:44:50 -0300

    Update UnidadesMedida.java
    Agregando comentarios

--- src/UnidadesMedida.java
+++ src/UnidadesMedida.java
@@ -1,6 +1,5 @@
-
-public class UnidadesMedida {
-	private int IdUnidades;
+public class UnidadesMedida {																	//Definción de clase									
+	private int IdUnidades;																		//Definición de atributos
 	private int UsuarioID;
 	private double Temperatura;
 	private double Humedad;
@@ -8,62 +7,46 @@ public class UnidadesMedida {
 	private double Viento;
 	private double Radiacion;
 	
-	public int obtenerIdUnidades () {
+	public int obtenerIdUnidades () {															//Definción de métodos constructores
 		return IdUnidades;
 	}
-	
-	public void establecerIdUnidades (int IDUnidades) {       //Ver si es necesario
+	public void establecerIdUnidades (int IDUnidades) {       
 		IdUnidades = IDUnidades;
 	}
-	
 	public int obtenerUsuarioID () {
 		return UsuarioID;
 	}
-	
-	public void establecerUsuarioID(int usuarioID) {			//Ver si es necesario
+	public void establecerUsuarioID(int usuarioID) {			
 		UsuarioID = usuarioID;		
 	}
-	
 	public double obtenerTemperatura () {
 		return Temperatura;
 	}
-	
 	public void establecerTemperatura (double temperatura) {
 		Temperatura = temperatura;
 	}
-	
 	public double obtenerHumedad() {
 		return Humedad;
 	}
-	
 	public void establecerHumedad(double humedad) {
 		Humedad = humedad;
 	}
-	
 	public double obtenerPrecipitacion() {
 		return Precipitacion;
 	}
-	
 	public void establecerPrecipitacion(double precipitacion) {
 		Precipitacion = precipitacion;
 	}
-	
 	public double obtenerViento() {
 		return Viento;
 	}
-	
 	public void establecerViento(double viento) {
 		Viento = viento;
 	}
-	
 	public double obtenerRadiacion() {
 		return Radiacion;
 	}
-	
 	public void establecerRadiacion(double radiacion) {
 		Radiacion = radiacion;
 	}
-	
-	
-
 }
